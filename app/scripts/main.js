d4449de2d1fd401d13ec733f6bbeba116076c590
zac1st1k <zacfirst@gmail.com>
2015-11-18 10:05:39 +1100

    Refactor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,130 +1,134 @@
-window.onload = function () {
+(function() {
 
-  init();
+  'use strict';
 
-};
+  window.onload = function () {
 
-function init () {
-
-  var currencyList = [
-
-    {"path": "currency.AUD.json"},
-    {"path": "currency.USD.json"},
-    {"path": "currency.GBP.json"},
-    {"path": "currency.ARS.json"},
-    {"path": "currency.BRL.json"},
-    {"path": "currency.CAD.json"},
-    {"path": "currency.CLP.json"},
-    {"path": "currency.CNY.json"},
-    {"path": "currency.HRK.json"},
-    {"path": "currency.CZK.json"},
-    {"path": "currency.DKK.json"},
-    {"path": "currency.EUR.json"},
-    {"path": "currency.HKD.json"},
-    {"path": "currency.HUF.json"},
-    {"path": "currency.INR.json"},
-    {"path": "currency.IDR.json"},
-    {"path": "currency.ILS.json"},
-    {"path": "currency.JPY.json"},
-    {"path": "currency.KWD.json"},
-    {"path": "currency.MYR.json"},
-    {"path": "currency.MXN.json"},
-    {"path": "currency.XPF.json"},
-    {"path": "currency.NZD.json"},
-    {"path": "currency.NOK.json"},
-    {"path": "currency.OMR.json"},
-    {"path": "currency.PLN.json"},
-    {"path": "currency.QAR.json"},
-    {"path": "currency.SAR.json"},
-    {"path": "currency.SGD.json"},
-    {"path": "currency.SBD.json"},
-    {"path": "currency.ZAR.json"},
-    {"path": "currency.KRW.json"},
-    {"path": "currency.LKR.json"},
-    {"path": "currency.SEK.json"},
-    {"path": "currency.CHF.json"},
-    {"path": "currency.TWD.json"},
-    {"path": "currency.THB.json"},
-    {"path": "currency.TRY.json"},
-    {"path": "currency.AED.json"},
-    {"path": "currency.VUV.json"},
-    {"path": "currency.VND.json"},
-    {"path": "currency.WST.json"}
-  ];
-
-  var startTime = new Date().getTime();
-
-  var table = document.getElementById('currency-table');
-
-  var updateTable = function (i, table) {
-
-    getNextFile(startTime, currencyList[i].path, populateTable, table, currencyList.length);
+    init();
 
   };
 
-  for (var i = 0; i < currencyList.length; i++) {
+  function init () {
 
-    updateTable(i, table);
+    var currencyList = [
 
-  }
+      {"path": "currency.AUD.json"},
+      {"path": "currency.USD.json"},
+      {"path": "currency.GBP.json"},
+      {"path": "currency.ARS.json"},
+      {"path": "currency.BRL.json"},
+      {"path": "currency.CAD.json"},
+      {"path": "currency.CLP.json"},
+      {"path": "currency.CNY.json"},
+      {"path": "currency.HRK.json"},
+      {"path": "currency.CZK.json"},
+      {"path": "currency.DKK.json"},
+      {"path": "currency.EUR.json"},
+      {"path": "currency.HKD.json"},
+      {"path": "currency.HUF.json"},
+      {"path": "currency.INR.json"},
+      {"path": "currency.IDR.json"},
+      {"path": "currency.ILS.json"},
+      {"path": "currency.JPY.json"},
+      {"path": "currency.KWD.json"},
+      {"path": "currency.MYR.json"},
+      {"path": "currency.MXN.json"},
+      {"path": "currency.XPF.json"},
+      {"path": "currency.NZD.json"},
+      {"path": "currency.NOK.json"},
+      {"path": "currency.OMR.json"},
+      {"path": "currency.PLN.json"},
+      {"path": "currency.QAR.json"},
+      {"path": "currency.SAR.json"},
+      {"path": "currency.SGD.json"},
+      {"path": "currency.SBD.json"},
+      {"path": "currency.ZAR.json"},
+      {"path": "currency.KRW.json"},
+      {"path": "currency.LKR.json"},
+      {"path": "currency.SEK.json"},
+      {"path": "currency.CHF.json"},
+      {"path": "currency.TWD.json"},
+      {"path": "currency.THB.json"},
+      {"path": "currency.TRY.json"},
+      {"path": "currency.AED.json"},
+      {"path": "currency.VUV.json"},
+      {"path": "currency.VND.json"},
+      {"path": "currency.WST.json"}
+    ];
 
-}
+    var startTime = new Date().getTime();
 
-function getNextFile (startTime, filePath, callback, table, progressBarValue) {
+    var table = document.getElementById('currency-table');
 
-  var xmlhttp = new XMLHttpRequest(), response;
+    for (var i = 0; i < currencyList.length; i++) {
 
-  xmlhttp.onreadystatechange = function () {
+      (function(i, table) {
 
-    if (xmlhttp.readyState == 4) {
+         return getNextFile(startTime, currencyList[i].path, populateTable, table, currencyList.length);
 
-      if (xmlhttp.status == 200) {
-
-        response = JSON.parse(xmlhttp.responseText);
-
-        callback(response, table, progressBarValue);
-
-      }
-
-      else {
-
-        response = false;
-
-      }
+      })(i, table);
 
     }
 
-  };
+  }
 
-  xmlhttp.open("GET", "assets/" + filePath + '?cachebuster=' + startTime, true);
+  function getNextFile (startTime, filePath, callback, table, progressBarValue) {
 
-  xmlhttp.send();
+    var xmlhttp = new XMLHttpRequest(), response;
 
-}
+    xmlhttp.onreadystatechange = function () {
 
-function populateTable (nextFile, table, progressBarValue) {
+      if (xmlhttp.readyState == 4) {
 
-  for (var i = 0; i < nextFile.currencies.length; i++) {
+        if (xmlhttp.status == 200) {
 
-    var row = document.createElement('tr');
+          response = JSON.parse(xmlhttp.responseText);
 
-    row.innerHTML =
-      '<td>' + nextFile.currencies[i].currencyName + '</td>' +
-      '<td>' + nextFile.currencies[i].currencyTitle + '</td>' +
-      '<td>' + parseFloat(nextFile.currencies[i].bbCashTChqs).toFixed(4) + '</td>' +
-      '<td>' + parseFloat(nextFile.currencies[i].bbForeignCheques).toFixed(4) + '</td>' +
-      '<td>' + parseFloat(nextFile.currencies[i].bbImt).toFixed(4) + '</td>' +
-      '<td>' + parseFloat(nextFile.currencies[i].bsCashTmcTChqs).toFixed(4) + '</td>' +
-      '<td>' + parseFloat(nextFile.currencies[i].bsImt).toFixed(4) + '</td>';
+          callback(response, table, progressBarValue);
 
-    table.appendChild(row);
+        }
+
+        else {
+
+          response = false;
+
+        }
+
+      }
+
+    };
+
+    xmlhttp.open("GET", "assets/" + filePath + '?cachebuster=' + startTime, true);
+
+    xmlhttp.send();
 
   }
 
-  var progressBarEl = document.getElementsByClassName('progress-bar')[0];
+  function populateTable (nextFile, table, progressBarValue) {
 
-  var style = window.getComputedStyle(progressBarEl, null);
+    for (var i = 0; i < nextFile.currencies.length; i++) {
 
-  progressBarEl.style.width = (parseFloat(style.width) + window.innerWidth/progressBarValue) + 'px';
+      var row = document.createElement('tr');
 
-}
+      row.innerHTML =
+        '<td>' + nextFile.currencies[i].currencyName + '</td>' +
+        '<td>' + nextFile.currencies[i].currencyTitle + '</td>' +
+        '<td>' + parseFloat(nextFile.currencies[i].bbCashTChqs).toFixed(4) + '</td>' +
+        '<td>' + parseFloat(nextFile.currencies[i].bbForeignCheques).toFixed(4) + '</td>' +
+        '<td>' + parseFloat(nextFile.currencies[i].bbImt).toFixed(4) + '</td>' +
+        '<td>' + parseFloat(nextFile.currencies[i].bsCashTmcTChqs).toFixed(4) + '</td>' +
+        '<td>' + parseFloat(nextFile.currencies[i].bsImt).toFixed(4) + '</td>';
+
+      table.appendChild(row);
+
+    }
+
+    var progressBarEl = document.getElementsByClassName('progress-bar')[0];
+
+    var style = window.getComputedStyle(progressBarEl, null);
+
+    progressBarEl.style.width = Math.round(parseFloat(style.width) + window.innerWidth/progressBarValue) + 'px';
+
+  }
+
+})();
